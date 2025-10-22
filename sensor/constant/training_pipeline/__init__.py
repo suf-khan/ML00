825801861ce94f038b6070d59ed8d63982badbbe
suf-khan <seeratulfatima.k@gmail.com>
2025-10-22 18:45:51 +0330

    model training

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -24,7 +24,7 @@ DATA_INGESTION_INGESTED_DIR: str= "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float=0.2
 
 """
-Data Validation realted contant start with DATA_VALIDATION VAR NAME
+Data Validation related constants start with DATA_VALIDATION VAR NAME
 """
 
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
@@ -34,10 +34,21 @@ DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 """
-Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+Data Transformation related constants start with DATA_TRANSFORMATION VAR NAME
 """
 
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+"""
+Model Trainer related constants start with MODE TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
 
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -7,11 +7,12 @@ from sensor.components.data_ingestion import DataIngestion
 
 from sensor.components.data_validation import DataValidation
 
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig, DataTransformationConfig
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 
 from sensor.components.data_transformation import DataTransformation
+from sensor.components.model_trainer import ModelTrainer
 
 
 
@@ -78,6 +79,17 @@ class TrainPipeline:
             return data_transformation_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
+        
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            model_trainer_config = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        except  Exception as e:
+            raise  SensorException(e,sys)
+        
+
      
 
 
@@ -96,6 +108,8 @@ class TrainPipeline:
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
 
             data_transformation_artifact= self.start_data_transformation(data_validation_artifact= data_validation_artifact)
+
+            model_trainer_artifact= self.start_model_trainer(data_transformation_artifact= data_transformation_artifact)
 
             
         except Exception as e :    
