c9c9251b46eebea7731e26276bae8dae9b306118
suf-khan <seeratulfatima.k@gmail.com>
2025-10-22 16:37:55 +0330

    data transformation

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -5,13 +5,13 @@ from sensor.logger import logging
 import sys , os 
 from sensor.components.data_ingestion import DataIngestion
 
-
 from sensor.components.data_validation import DataValidation
 
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig, DataTransformationConfig
 
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 
+from sensor.components.data_transformation import DataTransformation
 
 
 
@@ -58,8 +58,27 @@ class TrainPipeline:
         
         except  Exception as e:
             raise  SensorException(e,sys)
+        
 
 
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
+
+        try:
+
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config
+            )
+
+
+            data_transformation_artifact =  data_transformation.initiate_data_transformation()
+
+
+            return data_transformation_artifact
+        except  Exception as e:
+            raise  SensorException(e,sys)
+     
 
 
 
@@ -75,6 +94,8 @@ class TrainPipeline:
 
 
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
+
+            data_transformation_artifact= self.start_data_transformation(data_validation_artifact= data_validation_artifact)
 
             
         except Exception as e :    
